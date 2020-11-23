694b02b6a1f8d1299a794538a6b37adc93ed2a97
farevio <sagaraperkasa347@gmail.com>
2020-11-23 23:36:09 +0700

    Change SuperUsr to adminlte by Colorlib
    WIP, still a lot of mess

--- app/Http/Livewire/SuperUsr.php
+++ app/Http/Livewire/SuperUsr.php
@@ -2,16 +2,12 @@
 
 namespace App\Http\Livewire;
 
-use App\Apparel;
-use App\Product;
 use Livewire\Component;
 
 class SuperUsr extends Component
 {
     public function render()
     {
-        return view('livewire.super-usr')
-            ->extends('layouts.superapp')
-            ->section('content');    
+        return view('livewire.super-usr');
     }
 }
